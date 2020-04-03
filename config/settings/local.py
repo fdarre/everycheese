21b220abbe828dc1df4a7a4f808504907ed7717f
Frédéric Darré <frederic.darre@outlook.com>
2020-04-03 10:49:43 +0200

    Add comment about email backend in local settings

--- config/settings/local.py
+++ config/settings/local.py
@@ -25,6 +25,7 @@ CACHES = {
 # EMAIL
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+# pour display emails in the console during developpement
 EMAIL_BACKEND = env(
     "DJANGO_EMAIL_BACKEND",
     default="django.core.mail.backends.console.EmailBackend",
