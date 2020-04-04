1c75845036fcab0b7f0afc0583867dceeaf77da6
Frédéric Darré <frederic.darre@outlook.com>
2020-04-04 09:56:31 +0200

    Add Cheese model tests

--- everycheese/users/tests/test_models.py
+++ everycheese/users/tests/test_models.py
@@ -2,7 +2,7 @@ import pytest
 
 from everycheese.users.models import User
 
-pytestmark = pytest.mark.django_db
+pytestmark = pytest.mark.django_db  # drives the test database system.
 
 
 def test_user_get_absolute_url(user: User):
